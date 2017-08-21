327513d1ad96778355971910cae558dfe0bb356d
partrick.li <partrick.li@gmail.com>
2017-08-22 00:16:28 +0800

    detailed log view and controller added

---
+++ WorkDaily/LogDetailController.swift
@@ -0,0 +1,9 @@
+//
+//  LogDetailController.swift
+//  WorkDaily
+//
+//  Created by liguiyan on 2017/8/20.
+//  Copyright © 2017年 partrick. All rights reserved.
+//
+
+import Foundation
---
+++ WorkDaily/LogDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  LogDetailView.swift
+//  WorkDaily
+//
+//  Created by liguiyan on 2017/8/20.
+//  Copyright © 2017年 partrick. All rights reserved.
+//
+
+import Foundation
